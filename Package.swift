9f984b83c02028aa28dec8aea5a6e8534b6f66c2
Dejan Atanasov <39065925+atanasovdejan@users.noreply.github.com>
2025-04-12 00:45:48 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,15 +6,18 @@ let package = Package(
     platforms: [
         .iOS(.v15)
     ],
-    // dependencies: [
-    //     .package(url: "https://github.com/facebook/pop.git", from: "1.0.12")
-    // ],
+    dependencies: [
+       .package(url: "https://github.com/atanasovdejan/pop.git", branch: "master")
+    ],
     products: [
         .library(name: "FluidSlider",
             targets: ["FluidSlider"]),
     ],
     targets: [
         .target(name: "FluidSlider",
+                dependencies: [
+                .product(name: "pop", package: "pop")
+                ],
             path: "Sources")
     ]
 )
